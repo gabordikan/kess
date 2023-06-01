949111aa8f0f413dcee86279e25e5a23cd13e650
Gabor Dikan <gabordikan@gmail.com>
2023-06-01 20:09:31 +0200

    development

--- src/views/site/recordkess.php
+++ src/views/site/recordkess.php
@@ -13,42 +13,52 @@ use app\models\Penztarca;
 $this->title = 'Rögzítés';
 $this->params['breadcrumbs'][] = $this->title;
 
-$kategoriak = Kategoriak::getKategoriak();
-
+<?php
+if (Yii::$app->user->isGuest) {
 ?>
-<div class="site-recordkess">
-    <h1><?= Html::encode($this->title) ?></h1>
+    Lépjen be a funkciók eléréséhez
+<?php
+}
+else {
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'recordkess-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n{input}\n{error}",
-            'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-            'inputOptions' => ['class' => 'col-lg-3 form-control'],
-            'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-        ],
-    ]); ?>
+    $kategoriak = Kategoriak::getKategoriak();
 
-        <?= $form->field($model, 'datum')->widget(DatePicker::classname(), [
-            'dateFormat' => 'yyyy-MM-dd',
-        ]) ?>
+    ?>
+    <div class="site-recordkess">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-        <?= $form->field($model, 'penztarca_id')->dropDownList(
-            Penztarca::getPenztarcak(),
-            ['autofocus' => true]) ?>
+        <?php $form = ActiveForm::begin([
+            'id' => 'recordkess-form',
+            'layout' => 'horizontal',
+            'fieldConfig' => [
+                'template' => "{label}\n{input}\n{error}",
+                'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
+                'inputOptions' => ['class' => 'col-lg-3 form-control'],
+                'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+            ],
+        ]); ?>
 
-        <?= $form->field($model, 'kategoria_id')->dropDownList(
-                $kategoriak,
-            []) ?>
+            <?= $form->field($model, 'datum')->widget(DatePicker::classname(), [
+                'dateFormat' => 'yyyy-MM-dd',
+            ]) ?>
 
-        <?= $form->field($model, 'osszeg')->textInput() ?>
+            <?= $form->field($model, 'penztarca_id')->dropDownList(
+                Penztarca::getPenztarcak(),
+                ['autofocus' => true]) ?>
 
-        <div class="form-group">
-            <div class="offset-lg-1 col-lg-11">
-                <?= Html::submitButton('Mentés', ['class' => 'btn btn-primary', 'name' => 'save-button']) ?>
+            <?= $form->field($model, 'kategoria_id')->dropDownList(
+                    $kategoriak,
+                []) ?>
+
+            <?= $form->field($model, 'osszeg')->textInput() ?>
+
+            <div class="form-group">
+                <div class="offset-lg-1 col-lg-11">
+                    <?= Html::submitButton('Mentés', ['class' => 'btn btn-primary', 'name' => 'save-button']) ?>
+                </div>
             </div>
-        </div>
 
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); 
+}
+?>
 </div>
