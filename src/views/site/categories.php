f0f54899d2ddd746d100763e8a7399b937e135b7
Gabor Dikan <gabordikan@gmail.dom>
2023-06-03 12:57:42 +0200

    development - optimize buttons

--- src/views/site/categories.php
+++ src/views/site/categories.php
@@ -54,7 +54,7 @@ else {
 
         <?= $form->field($model, 'nev')->textInput() ?>
 
-        <?= $form->field($model, 'technikai')->checkbox([], []) ?>
+        <?= $form->field($model, 'technikai')->checkbox(["value"=>1])->label('Technikai') ?>
 
         <div class="form-group">
             <div">
